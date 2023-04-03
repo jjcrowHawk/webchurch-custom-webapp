06145c3e638c157bed98afc3570799e8d4f8d897
jjcrow <hawkcrowxx@gmail.com>
2023-04-02 21:36:40 -0500

    setting up project with MUI server-side css support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css'
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app'
+import theme from '../theme/theme';
+import createEmotionCache from '../utils/createEmotionCache';
+
+const emotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider value={emotionCache}>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+    </CacheProvider>
+  );
 }
---
+++ src/theme/theme.ts
@@ -0,0 +1,22 @@
+
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+  },
+});
+
+export default theme;
